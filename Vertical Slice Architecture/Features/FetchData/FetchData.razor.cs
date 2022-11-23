70a2a20d21daf6e5789c52ac80a48106b03e13ab
Marc Stigter <marcstigter2000@gmail.com>
2022-11-23 15:26:08 +0100

    initial commit

---
+++ Vertical Slice Architecture/Features/FetchData/FetchData.razor.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using Vertical_Slice_Architecture;
+using Vertical_Slice_Architecture.Shared;
+
+namespace Vertical_Slice_Architecture.Features.FetchData
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+    }
+}
---
+++ Vertical Slice Architecture/Features/FetchData/WeatherForecast.cs
@@ -0,0 +1,16 @@
+﻿namespace Vertical_Slice_Architecture.Features.FetchData
+{
+    public partial class FetchData
+    {
+        public class WeatherForecast
+        {
+            public DateTime Date { get; set; }
+
+            public int TemperatureC { get; set; }
+
+            public string? Summary { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
